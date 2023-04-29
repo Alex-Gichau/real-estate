4ad32634a46ff0362f9091f111158377be9e2244
Alex <gichaumburu@gmail.com>
2023-04-29 10:04:39 +0300

    Target blank tab for contacts

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Contacts.css';
 import { MdCall, MdFacebook } from 'react-icons/md';
-import { BsFillChatDotsFill, BsInstagram, BsTwitter, BsWhatsapp } from 'react-icons/bs';
+import { BsFillChatDotsFill, BsInstagram, BsMailbox, BsTwitter, BsWhatsapp } from 'react-icons/bs';
 import { HiChatBubbleBottomCenter } from 'react-icons/hi2';
 
 const Contacts = () => {
@@ -32,7 +32,7 @@ const Contacts = () => {
                   <span>Call Now</span>
                 </div>
               </div>
-              {/* Mode 2 */}
+              {/* Mode 2 - Whatsapp*/}
               <div className='flexColCenter mode'>
                 <div className='flexStart'>
                   <div className='flexCenter icon'>
@@ -44,14 +44,14 @@ const Contacts = () => {
                   </div>
                 </div>
                 <div className='flexCenter button'>
-                  <a href='https://wa.me/+254790479575'>
+                  <a href='https://wa.me/+254790479575' target="_blank">
                     <span>Message Now</span>
                   </a>
                 </div>
               </div>
             </div>
             <div className='flexStart row'>
-              {/* Mode 3 */}
+              {/* Mode 3  - Instagram*/}
               <div className='flexColCenter mode'>
                 <div className='flexStart'>
                   <div className='flexCenter icon'>
@@ -63,12 +63,12 @@ const Contacts = () => {
                   </div>
                 </div>
                 <div className='flexCenter button'>
-                  <a href='https://www.instagram.com'>
+                  <a href='https://www.instagram.com' target="_blank">
                     <span>Follow Us</span>
                   </a>
                 </div>
               </div>
-              {/* Mode 4 */}
+              {/* Mode 4 - Facebook*/}
               <div className='flexColCenter mode'>
                 <div className='flexStart'>
                   <div className='flexCenter icon'>
@@ -80,18 +80,18 @@ const Contacts = () => {
                   </div>
                 </div>
                 <div className='flexCenter button'>
-                  <a href='https://wa.me/+254790479575'>
+                  <a href='https://www.facebook.com' target="_blank">
                     <span>Like Us</span>
                   </a>
                 </div>
               </div>
             </div>
             <div className='flexStart row'>
-              {/* Mode 5 */}
+              {/* Mode 5 - Email*/}
               <div className='flexColCenter mode'>
                 <div className='flexStart'>
                   <div className='flexCenter icon'>
-                    <BsWhatsapp size={25} />
+                    <BsMailbox size={25} />
                   </div>
                   <div className='flexColStart detail'>
                     <span className='primaryText'>Email Us</span>
@@ -99,14 +99,13 @@ const Contacts = () => {
                   </div>
                 </div>
                 <div className='flexCenter button'>
-                  <a href='https://info@urbanstrategies.co.ke'>
+                  <a href='https://info@urbanstrategies.co.ke' target="_blank">
                     <span>Message Now</span>
                   </a>
                 </div>
               </div>
-              {/* Mode 6 */}
+              {/* Mode 6 - Twitter*/}
               <div className='flexStart row'>
-              {/* Mode 5 */}
               <div className='flexColCenter mode'>
                 <div className='flexStart'>
                   <div className='flexCenter icon'>
@@ -118,7 +117,7 @@ const Contacts = () => {
                   </div>
                 </div>
                 <div className='flexCenter button'>
-                  <a href='https://info@urbanstrategies.co.ke'>
+                  <a href='https://www.twitter.com' target="_blank">
                     <span>DM Now</span>
                   </a>
                 </div>
