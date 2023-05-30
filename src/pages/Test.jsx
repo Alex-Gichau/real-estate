9318d729b799a80e97741b333bc4469c08013fe1
Alex Gichau <geeshau@kali>
2023-05-30 15:45:00 +0300

    test loader changed

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -8,12 +8,9 @@ const login = () => {
     <section>
       <Header />
       <div className='flexColCenter innerWidth paddings'>
-        <img src='../../public/images/loader.gif' />
+        <img className='test-loader' src='../../public/images/loader3.gif'/>
         <span className='primaryText'>
-          Oops! We're still working on this. Return to{' '}
-          <NavLink className='./Home.jsx'>
-            <u>Home</u>
-          </NavLink>
+          Oops! We're still working on this.
         </span>
         <br />
         <span className='secondaryText'>Product of Geeshau&#8482;</span>
